e6ebc8e390ede17f275a0dcdb415981e7d021333
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2022-11-08 17:17:15 +0530

    changed cors

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
-);
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions));
 // Routes
 import { userRouter } from "./routes/userRoutes.js";
 import { taskRouter } from "./routes/taskRoutes.js";
